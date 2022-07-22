361ba631d2e82415439e82efdda9514ed90a4770
Chibuzor <chibuzorm26@example.com>
2022-07-22 21:00:12 +0100

    first commit

---
+++ src/screens/splash/Splash.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import logo from "../../assests/swoop.json";
+import Lottie from "lottie-react";
+import "./Splash.css";
+const Splash = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/Home");
+    }, 4500);
+  }, []);
+
+  return (
+    <div className="swoop">
+      <Lottie className="logo" animationData={logo} />
+    </div>
+  );
+};
+
+export default Splash;
